fad6cdfe8a30246a40c1f7029425fde656a8be27
Zachary Dennett <zach.dennett@gmail.com>
2012-10-19 17:06:49 -0400

    Finished tutorial with the exception of the bit about removing redunant views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
 # Copied from https://docs.djangoproject.com/en/dev/intro/tutorial03/
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import Context, loader
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
 
 
 from polls.models import Poll
@@ -17,7 +19,23 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render(request, 'polls/detail.html', {
+                      'poll': p,
+                      'error_message': "You didn't select a choice.",
+                      })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
