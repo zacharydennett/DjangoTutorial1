823ef1601e24d75e47f9d68b8381f7f46c2cf106
Zachary Dennett <zach.dennett@gmail.com>
2012-10-19 14:48:37 -0400

    Now have a working view and template

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,8 +7,11 @@ class Poll(models.Model):
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.question
-	def published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 		
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,1 +1,21 @@
 # Create your views here.
+# Copied from https://docs.djangoproject.com/en/dev/intro/tutorial03/
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.shortcuts import render
+
+from polls.models import Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html',context)
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s" % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
